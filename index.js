cb5bb4bca0a0715c473509cf66a6fd81e48d4adc
CHANDRAMUKHII <chandramukhiianbarasu@gmail.com>
2022-06-25 06:56:53 +0530

    new comments

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -27,6 +30,11 @@ app.get("/comments", (req, res) => {
 });
 app.get("/comments/new", (req, res) => {
   res.render("comments/new");
+});
+app.post("/comments", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.redirect("/comments");
 });
 app.get("/", (req, res) => {
   res.send("WoHooo, You are at he home page");
